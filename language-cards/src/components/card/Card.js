f66530d1331c9dbbb67a4667c39cb3afff1aa139
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-06-29 20:57:07 +0300

    development continues

--- language-cards/src/components/card/Card.js
+++ language-cards/src/components/card/Card.js
@@ -1,1 +1,19 @@
-import React from 'react';
+import { languages } from "../../helpers/data";
+
+const Card = () => {
+    return(
+        <div className="card-area-container">
+            <div className="bars"></div>
+            <h1 className="language-title">Languages</h1>
+            <div className="card-container">
+                {
+                languages.map((item, index) => {
+                    return <Item card={item}/>
+                })
+                }
+            </div>
+        </div>
+    )
+};
+
+export default Card;
